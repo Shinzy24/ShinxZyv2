25552bd85cf5694261ec4a21d306541ac982e1e6
Muhammad Zainul umam <93089530+Shinzy24@users.noreply.github.com>
2022-01-29 23:10:46 +0700

    Rename Random-ip.py to Random-ip.c

--- Random-ip.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-import ipaddr
-import random
- 
-network = ipaddr.IPv4Network('59.185.23.0/24)
-randmon_ip = ipaddr.IPv4Address(random.randrange(int(network.network) + 1, int(network.broadcast) - 1))
